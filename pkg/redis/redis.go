cbc1738b6b460fb79ada2d32828bea0de35756c4
qloog <quanlongwang@gmail.com>
2019-03-11 22:39:10 +0800

    add option

--- pkg/redis/redis.go
+++ pkg/redis/redis.go
@@ -10,9 +10,13 @@ var Client *redis.Client
 
 func init() {
 	Client = redis.NewClient(&redis.Options{
-		Addr:     viper.GetString("redis.addr"),
-		Password: viper.GetString("redis.password"),
-		DB:       viper.GetInt("redis.db"),
+		Addr:         viper.GetString("redis.addr"),
+		Password:     viper.GetString("redis.password"),
+		DB:           viper.GetInt("redis.db"),
+		DialTimeout:  viper.GetDuration("redis.dial_timeout"),
+		ReadTimeout:  viper.GetDuration("redis.read_timeout"),
+		WriteTimeout: viper.GetDuration("redis.write_timeout"),
+		PoolSize:     viper.GetInt("redis.pool_size"),
 	})
 
 	_, err := Client.Ping().Result()
