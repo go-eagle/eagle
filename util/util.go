77a70babce1aa5f538331ca6348d817828ad9296
qloog <quanlongwang@gmail.com>
2019-09-26 14:21:31 +0800

    add GenUUID

--- util/util.go
+++ util/util.go
@@ -2,11 +2,17 @@ package util
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/teris-io/shortid"
 )
 
 func GenShortId() (string, error) {
 	return shortid.Generate()
+}
+
+func GenUUID() string {
+	u, _ := uuid.NewRandom()
+	return u.String()
 }
 
 func GetReqID(c *gin.Context) string {
