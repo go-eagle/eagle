20ef36f2e13c9c6f4fb5361c154291174ab49dde
qloog <quanlongwang@gmail.com>
2019-10-30 10:15:45 +0800

    add ErrParam

--- pkg/errno/code.go
+++ pkg/errno/code.go
@@ -5,6 +5,7 @@ var (
 	OK                  = &Errno{Code: 0, Message: "OK"}
 	InternalServerError = &Errno{Code: 10001, Message: "Internal server error"}
 	ErrBind             = &Errno{Code: 10002, Message: "Error occurred while binding the request body to the struct."}
+	ErrParam            = &Errno{Code: 10003, Message: "Param error"}
 
 	ErrValidation = &Errno{Code: 20001, Message: "Validation failed."}
 	ErrDatabase   = &Errno{Code: 20002, Message: "Database error."}
