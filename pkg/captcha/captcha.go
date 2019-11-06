bf4c8fb416e12fda98fb3e3b7986b77f917114fe
qloog <quanlongwang@gmail.com>
2019-11-06 23:07:39 +0800

    fix

--- pkg/captcha/captcha.go
+++ pkg/captcha/captcha.go
@@ -1,13 +1,13 @@
 package captcha
 
-import "github.com/gin-gonic/gin"
-
-// see: https://github.com/wangsongyan/wblog/blob/master/controllers/captcha.go
-func CaptchaGet() {
-	session := sessions.Default(context)
-	captchaId := captcha.captcha.NewLen(4)
-	session.Delete(SESSION_CAPTCHA)
-	session.Set(SESSION_CAPTCHA, captchaId)SESSION_CAPTCHA, captchaId)
-	session.Save()
-	captcha.captcha.WriteImage(context.Writer, captchaId,  captchaId, 100, 40)
-}
+//import "github.com/gin-gonic/gin"
+//
+//// see: https://github.com/wangsongyan/wblog/blob/master/controllers/captcha.go
+//func CaptchaGet() {
+//	session := sessions.Default(context)
+//	captchaId := captcha.captcha.NewLen(4)
+//	session.Delete(SESSION_CAPTCHA)
+//	session.Set(SESSION_CAPTCHA, captchaId)SESSION_CAPTCHA, captchaId)
+//	session.Save()
+//	captcha.captcha.WriteImage(context.Writer, captchaId,  captchaId, 100, 40)
+//}
