db1e818515812591c1d615318942c2dbe2dcbf48
qloog <quanlongwang@gmail.com>
2019-11-18 22:50:12 +0800

    add

--- handler/user/get.go
+++ handler/user/get.go
@@ -1,9 +1,15 @@
 package user
 
 import (
+	"strconv"
+
+	"github.com/1024casts/snake/pkg/errno"
+
+	"github.com/pkg/errors"
+
 	. "github.com/1024casts/snake/handler"
 	"github.com/1024casts/snake/model"
-	"github.com/1024casts/snake/pkg/errno"
+	"github.com/1024casts/snake/service"
 
 	"github.com/gin-gonic/gin"
 	"github.com/lexkong/log"
@@ -20,13 +26,18 @@ import (
 func Get(c *gin.Context) {
 	log.Info("Get function called.")
 
-	username := c.Param("username")
-	// Get the user by the `username` from the database.
-	user, err := model.GetUser(username)
+	userIdStr := c.Param("user_id")
+	userId, _ := strconv.Atoi(userIdStr)
+
+	userModel := &model.UserModel{}
+	userModel.Id = uint64(userId)
+	userSrv := service.NewUserService()
+	err := userSrv.GetUserById(userModel)
 	if err != nil {
-		SendResponse(c, errno.ErrUserNotFound, nil)
+		log.Warnf("get user err, %v", errors.WithStack(err))
+		SendResponse(c, errno.InternalServerError, nil)
 		return
 	}
 
-	SendResponse(c, nil, user)
+	SendResponse(c, nil, userModel)
 }
