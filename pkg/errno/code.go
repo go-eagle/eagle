cdefdc77640beed650107965527a4cd350bff4ac
qloog <quanlongwang@gmail.com>
2019-12-04 13:39:03 +0800

    add ErrInvalidTransaction

--- pkg/errno/code.go
+++ pkg/errno/code.go
@@ -7,9 +7,10 @@ var (
 	ErrBind             = &Errno{Code: 10002, Message: "Error occurred while binding the request body to the struct."}
 	ErrParam            = &Errno{Code: 10003, Message: "Param error"}
 
-	ErrValidation = &Errno{Code: 20001, Message: "Validation failed."}
-	ErrDatabase   = &Errno{Code: 20002, Message: "Database error."}
-	ErrToken      = &Errno{Code: 20003, Message: "Error occurred while signing the JSON web token."}
+	ErrValidation         = &Errno{Code: 20001, Message: "Validation failed."}
+	ErrDatabase           = &Errno{Code: 20002, Message: "Database error."}
+	ErrToken              = &Errno{Code: 20003, Message: "Error occurred while signing the JSON web token."}
+	ErrInvalidTransaction = &Errno{Code: 20004, Message: "invalid transaction."}
 
 	// user errors
 	ErrEncrypt           = &Errno{Code: 20101, Message: "Error occurred while encrypting the user password."}
