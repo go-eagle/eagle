821e683bfed8984f00f94585de05ed709391e117
qloog <quanlongwang@gmail.com>
2020-03-23 20:44:21 +0800

    feat: remove redis options

--- pkg/redis/redis.go
+++ pkg/redis/redis.go
@@ -14,20 +14,17 @@ const Nil = redis.Nil
 
 func Init() {
 	Client = redis.NewClient(&redis.Options{
-		Addr:         viper.GetString("redis.addr"),
-		Password:     viper.GetString("redis.password"),
-		DB:           viper.GetInt("redis.db"),
-		DialTimeout:  viper.GetDuration("redis.dial_timeout"),
-		ReadTimeout:  viper.GetDuration("redis.read_timeout"),
-		WriteTimeout: viper.GetDuration("redis.write_timeout"),
-		PoolSize:     viper.GetInt("redis.pool_size"),
+		Addr:     viper.GetString("redis.addr"),
+		Password: viper.GetString("redis.password"),
+		DB:       viper.GetInt("redis.db"),
+		PoolSize: viper.GetInt("redis.pool_size"),
 	})
 
 	fmt.Println("redis addr:", viper.GetString("redis.addr"))
 
 	_, err := Client.Ping().Result()
 	if err != nil {
-		log.Errorf(err, "[redis] redis ping err: %+v")
+		log.Errorf(err, "[redis] redis ping err")
 		panic(err)
 	}
 }
