59fa83e83e59d501ea6c3e4da71a2aed2f6e46d2
qloog <quanlongwang@gmail.com>
2020-03-24 17:56:45 +0800

    fix: remove test func

--- pkg/log/logger_test.go
+++ pkg/log/logger_test.go
@@ -1,19 +1,1 @@
 package log
-
-import (
-	"testing"
-
-	"github.com/1024casts/snake/config"
-)
-
-func TestWriteLog(t *testing.T) {
-	// init config
-	if err := config.Init("../../conf/config.local.yaml"); err != nil {
-		panic(err)
-	}
-
-	l := InitLogger()
-	l.Warn("test warn")
-	l.Error("test error")
-	l.Info("test info")
-}
