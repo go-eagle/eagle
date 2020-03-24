2feb2ac61202a709b52ecd9be8cf46607da2e15a
qloog <quanlongwang@gmail.com>
2020-03-24 18:00:49 +0800

    feat: new add func

--- util/util.go
+++ util/util.go
@@ -1,9 +1,12 @@
 package util
 
 import (
+	"sync"
+
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"github.com/teris-io/shortid"
+	tnet "github.com/toolkits/net"
 )
 
 func GenShortId() (string, error) {
@@ -24,4 +27,22 @@ func GetReqID(c *gin.Context) string {
 		return requestId
 	}
 	return ""
+}
+
+var (
+	once     sync.Once
+	clientIP = "127.0.0.1"
+)
+
+//GetLocalIP 获取本地内网IP
+func GetLocalIP() string {
+	once.Do(func() {
+		ips, _ := tnet.IntranetIP()
+		if len(ips) > 0 {
+			clientIP = ips[0]
+		} else {
+			clientIP = "127.0.0.1"
+		}
+	})
+	return clientIP
 }
