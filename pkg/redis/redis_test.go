d16a3794a46851b38fc115e68b0b44337d010b8e
qloog <quanlongwang@gmail.com>
2020-03-24 18:19:12 +0800

    test: comment func

--- pkg/redis/redis_test.go
+++ pkg/redis/redis_test.go
@@ -1,22 +1,17 @@
 package redis
 
-import (
-	"testing"
-	"time"
-)
-
-func TestRedis(t *testing.T) {
-	Init()
-
-	var setGetKey = "test-set"
-	var setGetValue = "test-content"
-	Client.Set(setGetKey, setGetValue, time.Second*100)
-
-	expectValue := Client.Get(setGetKey).Val()
-	if setGetValue != expectValue {
-		t.Fail()
-		return
-	}
-
-	t.Log("redis set test pass")
-}
+//func TestRedis(t *testing.T) {
+//	Init()
+//
+//	var setGetKey = "test-set"
+//	var setGetValue = "test-content"
+//	Client.Set(setGetKey, setGetValue, time.Second*100)
+//
+//	expectValue := Client.Get(setGetKey).Val()
+//	if setGetValue != expectValue {
+//		t.Fail()
+//		return
+//	}
+//
+//	t.Log("redis set test pass")
+//}
