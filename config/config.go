a8984d4eb366e3c1926bb456b3fe7d1698a94bbc
qloog <quanlongwang@gmail.com>
2020-04-06 08:22:54 +0800

    feat: modify default config name and add comment

--- config/config.go
+++ config/config.go
@@ -9,10 +9,12 @@ import (
 	"github.com/spf13/viper"
 )
 
+// Config 读取配置
 type Config struct {
 	Name string
 }
 
+// Init 初始化配置，默认读取config.local.yaml
 func Init(cfg string) error {
 	c := Config{
 		Name: cfg,
@@ -37,7 +39,7 @@ func (cfg *Config) initConfig() error {
 		viper.SetConfigFile(cfg.Name) // 如果指定了配置文件，则解析指定的配置文件
 	} else {
 		viper.AddConfigPath("conf") // 如果没有指定配置文件，则解析默认的配置文件
-		viper.SetConfigName("config")
+		viper.SetConfigName("config.local")
 	}
 	viper.SetConfigType("yaml") // 设置配置文件格式为YAML
 	viper.AutomaticEnv()        // 读取匹配的环境变量
