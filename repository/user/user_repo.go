5a05cf6e23898047c81bcb4556b355b6f8fa5ee3
qloog <quanlongwang@gmail.com>
2020-04-19 22:50:31 +0800

    chore: optimize func

--- repository/user/user_repo.go
+++ repository/user/user_repo.go
@@ -1,4 +1,4 @@
-package repository
+package user
 
 import (
 	"github.com/1024casts/snake/model"
@@ -9,12 +9,12 @@ import (
 
 // UserRepo 定义用户仓库接口
 type UserRepo interface {
-	CreateUser(db *gorm.DB, user model.UserModel) (id uint64, err error)
+	Create(db *gorm.DB, user model.UserModel) (id uint64, err error)
+	Update(db *gorm.DB, id uint64, userMap map[string]interface{}) error
 	GetUserByID(id uint64) (*model.UserModel, error)
 	GetUserByPhone(phone int) (*model.UserModel, error)
 	GetUserByEmail(email string) (*model.UserModel, error)
 	GetUsersByIds(ids []uint64) ([]*model.UserModel, error)
-	Update(userMap map[string]interface{}, id uint64) error
 }
 
 // userRepo 用户仓库
@@ -25,14 +25,24 @@ func NewUserRepo() UserRepo {
 	return &userRepo{}
 }
 
-// CreateUser 创建用户
-func (repo *userRepo) CreateUser(db *gorm.DB, user model.UserModel) (id uint64, err error) {
+// Create 创建用户
+func (repo *userRepo) Create(db *gorm.DB, user model.UserModel) (id uint64, err error) {
 	err = db.Create(&user).Error
 	if err != nil {
 		return 0, err
 	}
 
 	return user.ID, nil
+}
+
+// Update 更新用户信息
+func (repo *userRepo) Update(db *gorm.DB, id uint64, userMap map[string]interface{}) error {
+	user, err := repo.GetUserByID(id)
+	if err != nil {
+		return err
+	}
+
+	return db.Model(user).Updates(userMap).Error
 }
 
 // GetUserByID 获取用户
@@ -69,14 +79,4 @@ func (repo *userRepo) GetUsersByIds(ids []uint64) ([]*model.UserModel, error) {
 	result := model.GetDB().Where("id in (?)", ids).Find(&users)
 
 	return users, result.Error
-}
-
-// Update 更新用户信息
-func (repo *userRepo) Update(userMap map[string]interface{}, id uint64) error {
-	user, err := repo.GetUserByID(id)
-	if err != nil {
-		return err
-	}
-
-	return model.GetDB().Model(user).Updates(userMap).Error
 }
