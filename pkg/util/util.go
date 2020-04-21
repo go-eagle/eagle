8adc5622ada12977da15b46dda96a2f6c0cef15a
qloog <quanlongwang@gmail.com>
2020-04-21 14:03:09 +0800

    chore: update comment

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -16,7 +16,7 @@ func GenShortID() (string, error) {
 	return shortid.Generate()
 }
 
-// GenUUID 生成随机字符串
+// GenUUID 生成随机字符串，eg: 76d27e8c-a80e-48c8-ad20-e5562e0f67e4
 func GenUUID() string {
 	u, _ := uuid.NewRandom()
 	return u.String()
