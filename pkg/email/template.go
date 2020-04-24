dca0bb7ce8270e0060cbfaf6e190346516efe790
qloog <quanlongwang@gmail.com>
2020-04-24 23:43:37 +0800

    feat: add func

--- pkg/email/template.go
+++ pkg/email/template.go
@@ -1,1 +1,6 @@
 package email
+
+// NewActivationEmail 发送激活邮件
+func NewActivationEmail(username, activateURL string) (subject string, body string) {
+	return "帐号激活链接", "Hi, " + username + "<br>请激活您的帐号： <a href = '" + activateURL + "'>" + activateURL + "</a>"
+}
