c9c03396b8bf9ad351c2cba454e0a55cee3ce591
qloog <quanlongwang@gmail.com>
2020-05-03 00:26:29 +0800

    test: add func TestInitLogger

--- pkg/log/logger_test.go
+++ pkg/log/logger_test.go
@@ -1,1 +1,20 @@
 package log
+
+import (
+	"testing"
+
+	"github.com/1024casts/snake/config"
+)
+
+func TestInitLogger(t *testing.T) {
+	// init config
+	if err := config.Init("../../conf/config.local.yaml"); err != nil {
+		panic(err)
+	}
+
+	logger := InitLogger()
+
+	logger.Info("test logger info")
+	logger.Warn("test logger warning")
+	logger.Error("test logger error")
+}
