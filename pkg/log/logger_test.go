8825eb1803ed802b0026939563c349ad9af5bcdb
qloog <quanlongwang@gmail.com>
2020-05-03 10:36:05 +0800

    chore: modify func name to NewLogger from InitLogger

--- pkg/log/logger_test.go
+++ pkg/log/logger_test.go
@@ -6,13 +6,13 @@ import (
 	"github.com/1024casts/snake/config"
 )
 
-func TestInitLogger(t *testing.T) {
+func TestNewLogger(t *testing.T) {
 	// init config
 	if err := config.Init("../../conf/config.local.yaml"); err != nil {
 		panic(err)
 	}
 
-	logger := InitLogger()
+	logger := NewLogger()
 
 	logger.Info("test logger info")
 	logger.Warn("test logger warning")
