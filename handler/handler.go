41e44357741f095f9ce8c3e72382b9b6f25040be
qloog <quanlongwang@gmail.com>
2020-05-07 23:53:41 +0800

    feat: add hostname

--- handler/handler.go
+++ handler/handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/1024casts/snake/pkg/errno"
 
@@ -50,12 +51,22 @@ func RouteNotFound(c *gin.Context) {
 	c.String(http.StatusNotFound, "The incorrect API route.")
 }
 
+// getHostname 获取主机名
+func getHostname() string {
+	name, err := os.Hostname()
+	if err != nil {
+		name = "unknown"
+	}
+	return name
+}
+
 // healthCheckResponse 健康检查响应结构体
 type healthCheckResponse struct {
-	Status string `json:"status"`
+	Status   string `json:"status"`
+	Hostname string `json:"hostname"`
 }
 
 // HealthCheck will return OK if the underlying BoltDB is healthy. At least healthy enough for demoing purposes.
 func HealthCheck(c *gin.Context) {
-	c.JSON(http.StatusOK, healthCheckResponse{Status: "UP"})
+	c.JSON(http.StatusOK, healthCheckResponse{Status: "UP", Hostname: getHostname()})
 }
