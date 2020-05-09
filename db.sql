d0d12f5b7637dc55602ebc06f055c250cf57982e
qloog <quanlongwang@gmail.com>
2020-05-09 10:31:07 +0800

    sql: update table and add test data

--- db.sql
+++ db.sql
@@ -9,13 +9,21 @@ USE `snake`;
 DROP TABLE IF EXISTS `tb_users`;
 
 CREATE TABLE `tb_users` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(30) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `deleted_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uniq_username` (`username`),
-  KEY `idx_tb_users_deletedAt` (`deleted_at`)
-) ENGINE=Innodb DEFAULT CHARSET=utf8;
+     `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+     `username` varchar(255) NOT NULL DEFAULT '',
+     `password` varchar(60) NOT NULL DEFAULT '',
+     `avatar` varchar(255) NOT NULL DEFAULT '' COMMENT '头像',
+     `phone` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '手机号',
+     `sex` tinyint(4) unsigned NOT NULL DEFAULT '0' COMMENT '性别 0:未知 1:男 2:女',
+     `deleted_at` timestamp NULL DEFAULT NULL,
+     `created_at` timestamp NULL DEFAULT NULL,
+     `updated_at` timestamp NULL DEFAULT NULL,
+     PRIMARY KEY (`id`),
+     UNIQUE KEY `uniq_username` (`username`),
+     UNIQUE KEY `uniq_phone` (`phone`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
+
+INSERT INTO `tb_users` (`id`, `username`, `password`, `avatar`, `phone`, `sex`, `created_at`, `updated_at`,`deleted_at`)
+VALUES
+(null, 'test-name', '6BB4837EB74329105EE4568DDA7DC67ED2CA2AD9', '/uploads/avatar.jpg', 13810002001, 1, '2020-02-09 10:23:33', '2020-05-09 10:23:33', NULL);
+
