2cc20e096934ad9d93e627ce6eefd4e80f235247
qloog <quanlongwang@gmail.com>
2020-05-29 12:58:43 +0800

    chore: optimize err log and import

--- repository/user/user_repo.go
+++ repository/user/user_repo.go
@@ -3,9 +3,8 @@ package user
 import (
 	"time"
 
-	"github.com/1024casts/snake/pkg/log"
-
 	"github.com/jinzhu/gorm"
+	"github.com/pkg/errors"
 
 	"github.com/1024casts/snake/model"
 )
@@ -36,7 +35,7 @@ func NewUserRepo() Repo {
 func (repo *userRepo) Create(db *gorm.DB, user model.UserModel) (id uint64, err error) {
 	err = db.Create(&user).Error
 	if err != nil {
-		return 0, err
+		return 0, errors.Wrap(err, "[user_repo] create user err")
 	}
 
 	return user.ID, nil
@@ -46,7 +45,7 @@ func (repo *userRepo) Create(db *gorm.DB, user model.UserModel) (id uint64, err 
 func (repo *userRepo) Update(db *gorm.DB, id uint64, userMap map[string]interface{}) error {
 	user, err := repo.GetUserByID(db, id)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "[user_repo] update user data err")
 	}
 
 	return db.Model(user).Updates(userMap).Error
@@ -57,7 +56,7 @@ func (repo *userRepo) GetUserByID(db *gorm.DB, id uint64) (*model.UserModel, err
 	user := &model.UserModel{}
 	err := db.Where("id = ?", id).First(user).Error
 	if err != nil && err != gorm.ErrRecordNotFound {
-		return user, err
+		return user, errors.Wrap(err, "[user_repo] get user data err")
 	}
 
 	return user, nil
@@ -66,47 +65,67 @@ func (repo *userRepo) GetUserByID(db *gorm.DB, id uint64) (*model.UserModel, err
 // GetUserByPhone 根据手机号获取用户
 func (repo *userRepo) GetUserByPhone(db *gorm.DB, phone int) (*model.UserModel, error) {
 	user := model.UserModel{}
-	result := db.Where("phone = ?", phone).First(&user)
+	err := db.Where("phone = ?", phone).First(&user).Error
+	if err != nil {
+		return nil, errors.Wrap(err, "[user_repo] get user err by phone")
+	}
 
-	return &user, result.Error
+	return &user, nil
 }
 
 // GetUserByEmail 根据邮箱获取手机号
 func (repo *userRepo) GetUserByEmail(db *gorm.DB, phone string) (*model.UserModel, error) {
 	user := model.UserModel{}
-	result := db.Where("email = ?", phone).First(&user)
+	err := db.Where("email = ?", phone).First(&user).Error
+	if err != nil {
+		return nil, errors.Wrap(err, "[user_repo] get user err by email")
+	}
 
-	return &user, result.Error
+	return &user, nil
 }
 
 // GetUsersByIds 批量获取用户
 func (repo *userRepo) GetUsersByIds(ids []uint64) ([]*model.UserModel, error) {
 	users := make([]*model.UserModel, 0)
-	result := model.GetDB().Where("id in (?)", ids).Find(&users)
+	err := model.GetDB().Where("id in (?)", ids).Find(&users).Error
+	if err != nil {
+		return nil, errors.Wrap(err, "[user_repo] get user err by phone")
+	}
 
-	return users, result.Error
+	return users, nil
 }
 
 // IncrFollowCount 增加关注数
 func (repo *userRepo) IncrFollowCount(db *gorm.DB, userID uint64, step int) error {
-	return db.Exec("insert into user_stat set user_id=?, follow_count=1, created_at=? on duplicate key update "+
+	err := db.Exec("insert into user_stat set user_id=?, follow_count=1, created_at=? on duplicate key update "+
 		"follow_count=follow_count+?, updated_at=?",
 		userID, time.Now(), step, time.Now()).Error
+	if err != nil {
+		return errors.Wrap(err, "[user_repo] incr user follow count")
+	}
+	return nil
 }
 
 // IncrFollowerCount 增加粉丝数
 func (repo *userRepo) IncrFollowerCount(db *gorm.DB, userID uint64, step int) error {
-	return db.Exec("insert into user_stat set user_id=?, follower_count=1, created_at=? on duplicate key update "+
+	err := db.Exec("insert into user_stat set user_id=?, follower_count=1, created_at=? on duplicate key update "+
 		"follower_count=follower_count+?, updated_at=?",
 		userID, time.Now(), step, time.Now()).Error
+	if err != nil {
+		return errors.Wrap(err, "[user_repo] incr user follower count")
+	}
+	return nil
 }
 
 // GetUserStatByID 获取用户统计数据
 func (repo *userRepo) GetUserStatByID(db *gorm.DB, userID uint64) (*model.UserStatModel, error) {
 	userStat := model.UserStatModel{}
-	result := db.Where("user_id = ?", userID).First(&userStat)
+	err := db.Where("user_id = ?", userID).First(&userStat).Error
+	if err != nil {
+		return nil, errors.Wrap(err, "[user_repo] get user stat err")
+	}
 
-	return &userStat, result.Error
+	return &userStat, nil
 }
 
 // GetUserStatByIDs 批量获取用户统计数据
@@ -116,8 +135,7 @@ func (repo *userRepo) GetUserStatByIDs(db *gorm.DB, userID []uint64) (map[uint64
 
 	result := model.GetDB().Where("user_id in (?)", userID).Find(&userStats)
 	if err := result.Error; err != nil {
-		log.Warnf("[user_follow] get user follow err, %v", err)
-		return retMap, err
+		return retMap, errors.Wrap(err, "[user_repo] get user stat err")
 	}
 
 	for _, v := range userStats {
