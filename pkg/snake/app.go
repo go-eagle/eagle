c42001502e0103be532a519c75e9d7ad3d278711
qloog <quanlongwang@gmail.com>
2020-08-21 01:23:28 +0800

    chore: use std log

--- pkg/snake/app.go
+++ pkg/snake/app.go
@@ -2,6 +2,7 @@ package snake
 
 import (
 	"context"
+	"log"
 	"net/http"
 	"os"
 	"os/signal"
@@ -12,7 +13,6 @@ import (
 	redis2 "github.com/1024casts/snake/pkg/redis"
 
 	"github.com/1024casts/snake/internal/model"
-	"github.com/1024casts/snake/pkg/log"
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis"
 	"github.com/jinzhu/gorm"
@@ -65,7 +65,7 @@ func New(cfg *conf.Config) *Application {
 
 // Run start a app
 func (a *Application) Run() {
-	log.Infof("Start to listening the incoming requests on http address: %s", viper.GetString("app.addr"))
+	log.Printf("Start to listening the incoming requests on http address: %s", viper.GetString("app.addr"))
 	srv := &http.Server{
 		Addr:    viper.GetString("app.addr"),
 		Handler: a.Router,
@@ -82,26 +82,22 @@ func (a *Application) Run() {
 
 // gracefulStop 优雅退出
 // 等待中断信号以超时 5 秒正常关闭服务器
-// 官方说明：https://github.com/gin-gonic/gin#graceful-restart-or-stop
+// 官方说明：https://github.com/gin-gonic/gin#graceful-shutdown-or-restart
 func gracefulStop(srv *http.Server) {
 	quit := make(chan os.Signal)
-	// kill 命令发送信号 syscall.SIGTERM
-	// kill -2 命令发送信号 syscall.SIGINT
-	// kill -9 命令发送信号 syscall.SIGKILL
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
-	log.Info("Shutdown Server ...")
+	log.Println("Shutting down server...")
 
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatal("Server Shutdown:", err)
+		log.Fatal("Server forced to shutdown:", err)
 	}
-	// 5 秒后捕获 ctx.Done() 信号
-	select {
-	case <-ctx.Done():
-		log.Info("timeout of 5 seconds.")
-	default:
-	}
-	log.Info("Server exiting")
+	log.Println("Server exiting")
 }
