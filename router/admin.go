482bb7605ae187dd8572dde0c8aa11076a09b8aa
qloog <quanlongwang@gmail.com>
2020-10-08 22:56:31 +0800

    feat: init admin router

---
+++ router/admin.go
@@ -0,0 +1,10 @@
+package routers
+
+import "github.com/gin-gonic/gin"
+
+// LoadAdminRouter loads the middlewares, routes, handlers.
+func LoadAdminRouter(g *gin.Engine) *gin.Engine {
+	router := g
+
+	return router
+}
