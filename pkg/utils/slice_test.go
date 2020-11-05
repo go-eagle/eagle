d8c205b3a6e098f381f65360021d9b0d937f7c03
Richard <quanlongwang@gmail.com>
2020-11-05 22:23:51 +0800

    chore: add slice append benchmark

--- pkg/utils/slice_test.go
+++ pkg/utils/slice_test.go
@@ -243,3 +243,78 @@ func TestIsInSlice(t *testing.T) {
 		})
 	}
 }
+
+// go test -bench="."
+// benchamark slice compare
+type Something struct {
+	roomId   int
+	roomName string
+}
+
+// BenchmarkDefaultSlice default slice
+func BenchmarkDefaultSlice(b *testing.B) {
+	b.ReportAllocs()
+	var wg sync.WaitGroup
+	for i := 0; i < b.N; i++ {
+		wg.Add(1)
+		go func(wg *sync.WaitGroup) {
+			for i := 0; i < 120; i++ {
+				output := make([]Something, 0)
+				output = append(output, Something{
+					roomId:   i,
+					roomName: strconv.Itoa(i),
+				})
+			}
+			wg.Done()
+		}(&wg)
+	}
+	wg.Wait()
+}
+
+// BenchmarkPreAllocSlice 预分配
+func BenchmarkPreAllocSlice(b *testing.B) {
+	b.ReportAllocs()
+	var wg sync.WaitGroup
+
+	for i := 0; i < b.N; i++ {
+		wg.Add(1)
+		go func(wg *sync.WaitGroup) {
+			output := make([]Something, 0, 120)
+			for i := 0; i < 120; i++ {
+				output = append(output, Something{
+					roomId:   i,
+					roomName: strconv.Itoa(i),
+				})
+			}
+			wg.Done()
+		}(&wg)
+	}
+	wg.Wait()
+
+}
+
+// BenchmarkSyncPoolSlice 使用 sync pool
+func BenchmarkSyncPoolSlice(b *testing.B) {
+	b.ReportAllocs()
+	var wg sync.WaitGroup
+	var SomethingPool = sync.Pool{
+		New: func() interface{} {
+			b := make([]Something, 120)
+			return &b
+		},
+	}
+	for i := 0; i < b.N; i++ {
+		wg.Add(1)
+		go func(wg *sync.WaitGroup) {
+			obj := SomethingPool.Get().(*[]Something)
+			for i := 0; i < 120; i++ {
+				some := *obj
+				some[i].roomId = i
+				some[i].roomName = strconv.Itoa(i)
+			}
+			SomethingPool.Put(obj)
+			wg.Done()
+		}(&wg)
+	}
+	wg.Wait()
+}
