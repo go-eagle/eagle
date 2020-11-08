67a9a5c094486b6606d4105ea5bd187bdb110391
qloog <quanlongwang@gmail.com>
2020-11-08 23:05:06 +0800

    feat: add slice queue

---
+++ pkg/queue/slice_queue.go
@@ -0,0 +1,32 @@
+package queue
+
+import "sync"
+
+type SliceQueue struct {
+	data []interface{}
+	mu   sync.Mutex
+}
+
+func NewSliceQueue(n int) (q *SliceQueue) {
+	return &SliceQueue{data: make([]interface{}, 0, n)}
+}
+
+// Enqueue 把值放在队尾
+func (q *SliceQueue) Enqueue(v interface{}) {
+	q.mu.Lock()
+	q.data = append(q.data, v)
+	q.mu.Unlock()
+}
+
+// Dequeue 移去队头并返回
+func (q *SliceQueue) Dequeue() interface{} {
+	q.mu.Lock()
+	if len(q.data) == 0 {
+		q.mu.Unlock()
+		return nil
+	}
+	v := q.data[0]
+	q.data = q.data[1:]
+	q.mu.Unlock()
+	return v
+}
