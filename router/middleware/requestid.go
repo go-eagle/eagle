9e070b9aca3efd373193e106445fcaded500b2ec
qloog <quanlongwang@gmail.com>
2020-11-26 10:37:51 +0800

    chore: use request_id pkg in middleware

--- router/middleware/requestid.go
+++ router/middleware/requestid.go
@@ -3,27 +3,27 @@ package middleware
 import (
 	"github.com/gin-gonic/gin"
 
-	"github.com/1024casts/snake/pkg/utils"
+	"github.com/1024casts/snake/pkg/requestid"
 )
 
-// RequestID 透传Request-ID，如果没有则生成一个
+// RequestID request id middleware
 func RequestID() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// Check for incoming header, use it if exists
-		requestID := c.Request.Header.Get(utils.HTTPHeaderXRequestIDKey)
+		requestID := c.Request.Header.Get(requestid.HeaderXRequestIDKey)
 
 		// Create request id with UUID
 		if requestID == "" {
-			requestID = utils.GenRequestID()
-			ctx := utils.NewRequestIDContext(c.Request.Context(), requestID)
+			requestID = requestid.Generate()
+			ctx := requestid.NewContext(c.Request.Context(), requestID)
 			c.Request = c.Request.WithContext(ctx)
 		}
 
 		// Expose it for use in the application
-		c.Set(utils.ContextRequestIDKey, requestID)
+		c.Set(requestid.ContextRequestIDKey, requestID)
 
 		// Set X-Request-ID header
-		c.Writer.Header().Set(utils.HTTPHeaderXRequestIDKey, requestID)
+		c.Writer.Header().Set(requestid.HeaderXRequestIDKey, requestID)
 
 		c.Next()
 	}
