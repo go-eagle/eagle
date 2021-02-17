9f7dfaed4b4b82122ce34e1d066196e27a900e91
qloog <quanlongwang@gmail.com>
2021-02-17 22:38:57 +0800

    chore: add param

--- pkg/net/tracing/span.go
+++ pkg/net/tracing/span.go
@@ -9,7 +9,7 @@ import (
 
 // StartSpanFromRequest extracts the parent span context from the inbound HTTP request
 // and starts a new child span if there is a parent span.
-func StartSpanFromRequest(tracer opentracing.Tracer, r *http.Request) opentracing.Span {
+func StartSpanFromRequest(operationName string, tracer opentracing.Tracer, r *http.Request) opentracing.Span {
 	spanCtx, _ := Extract(tracer, r)
-	return tracer.StartSpan("ping-receive", ext.RPCServerOption(spanCtx))
+	return tracer.StartSpan(operationName, ext.RPCServerOption(spanCtx))
 }
