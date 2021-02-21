442ffe0a836b81677a397c20764eae16847a072f
qloog <quanlongwang@gmail.com>
2021-02-21 13:47:42 +0800

    feat: add tag for tracing

--- router/middleware/trace.go
+++ router/middleware/trace.go
@@ -8,27 +8,46 @@ import (
 	"github.com/1024casts/snake/pkg/net/tracing"
 )
 
+const (
+	// DefaultServiceName service name
+	DefaultServiceName = "snake"
+)
+
 func Trace() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		tracer, closer := tracing.Init("snake")
+		tracer, closer := tracing.Init(DefaultServiceName)
 		defer closer.Close()
 
 		// set into opentracing
 		opentracing.SetGlobalTracer(tracer)
 
-		parentSpanCtx, err := tracing.Extract(tracer, c.Request)
+		var sp opentracing.Span
+		clientSpanCtx, err := tracing.Extract(tracer, c.Request)
 		if err != nil {
-			parentSpan := tracer.StartSpan(c.Request.URL.Path)
-			defer parentSpan.Finish()
+			// root span
+			sp = tracer.StartSpan(c.Request.URL.Path)
+			sp.SetTag("error", err)
 		} else {
-			childSpan := tracer.StartSpan(
+			// child span
+			sp = tracer.StartSpan(
 				c.Request.URL.Path,
-				opentracing.ChildOf(parentSpanCtx),
+				opentracing.ChildOf(clientSpanCtx),
 				opentracing.Tag{Key: string(ext.Component), Value: "HTTP"},
 				ext.SpanKindRPCServer,
 			)
-			defer childSpan.Finish()
 		}
+		defer sp.Finish()
+
+		// record HTTP method
+		ext.HTTPMethod.Set(sp, c.Request.Method)
+		// record HTTP url
+		ext.HTTPUrl.Set(sp, c.Request.URL.String())
+		// record component name
+		// ext.Component.Set(sp, componentName)
+
 		c.Next()
+
+		// record HTTP status code
+		ext.HTTPStatusCode.Set(sp, uint16(c.Writer.Status()))
 	}
 }
