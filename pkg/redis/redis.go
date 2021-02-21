e4e229c77f1dcd9df3416a0a7d7bf993a00309b7
qloog <quanlongwang@gmail.com>
2021-02-21 21:18:40 +0800

    chore: revert redis client

--- pkg/redis/redis.go
+++ pkg/redis/redis.go
@@ -1,19 +1,17 @@
 package redis
 
 import (
-	"context"
 	"fmt"
 
 	"github.com/alicebob/miniredis"
 	"github.com/go-redis/redis"
-	apmgoredis "github.com/opentracing-contrib/goredis"
 
 	"github.com/1024casts/snake/pkg/conf"
 	"github.com/1024casts/snake/pkg/log"
 )
 
 // RedisClient redis 客户端
-var RedisClient redis.UniversalClient
+var RedisClient *redis.Client
 
 // Nil redis 返回为空
 const Nil = redis.Nil
@@ -22,10 +20,10 @@ const Nil = redis.Nil
 const ErrRedisNotFound = redis.Nil
 
 // Init 实例化一个redis client
-func Init(cfg *conf.Config) redis.UniversalClient {
+func Init(cfg *conf.Config) *redis.Client {
 	c := cfg.Redis
-	RedisClient = redis.NewUniversalClient(&redis.UniversalOptions{
-		Addrs:        []string{c.Addr},
+	RedisClient = redis.NewClient(&redis.Options{
+		Addr:         c.Addr,
 		Password:     c.Password,
 		DB:           c.Db,
 		MinIdleConns: c.MinIdleConn,
@@ -35,9 +33,6 @@ func Init(cfg *conf.Config) redis.UniversalClient {
 		PoolSize:     c.PoolSize,
 		PoolTimeout:  c.PoolTimeout,
 	})
-
-	// TODO: 使用每一次请求的context
-	RedisClient = apmgoredis.Wrap(RedisClient).WithContext(context.Background())
 
 	_, err := RedisClient.Ping().Result()
 	if err != nil {
