d0b4f0e3597f802a4fb2d5302d76dcedc4845d35
qloog <quanlongwang@gmail.com>
2021-02-22 00:13:45 +0800

    docs: add more comment

--- pkg/net/tracing/span.go
+++ pkg/net/tracing/span.go
@@ -8,7 +8,7 @@ import (
 )
 
 // StartSpanFromRequest extracts the parent span context from the inbound HTTP request
-// and starts a new child span if there is a parent span.
+// and starts a new child span or root if there is a parent span.
 func StartSpanFromRequest(operationName string, tracer opentracing.Tracer, r *http.Request) opentracing.Span {
 	spanCtx, _ := Extract(tracer, r)
 	return tracer.StartSpan(operationName, ext.RPCServerOption(spanCtx))
