6f7d4633a1eb802014dff262922a7ac9e975d07d
quanlong <wangquanlong@joyme.sg>
2021-03-08 20:19:43 +0800

    chore: move opendb to pkg

--- internal/model/init.go
+++ internal/model/init.go
@@ -1,16 +1,10 @@
 package model
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
-
-	// MySQL driver.
-	"gorm.io/driver/mysql"
-	// GORM MySQL
 	"gorm.io/gorm"
 
 	"github.com/1024casts/snake/pkg/conf"
+	"github.com/1024casts/snake/pkg/database/orm"
 )
 
 // DB 数据库全局变量
@@ -18,41 +12,8 @@ var DB *gorm.DB
 
 // Init 初始化数据库
 func Init(cfg *conf.Config) *gorm.DB {
-	return openDB(cfg)
-}
-
-// openDB 链接数据库，生成数据库实例
-func openDB(cfg *conf.Config) *gorm.DB {
-	c := cfg.MySQL
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=%t&loc=%s",
-		c.UserName,
-		c.Password,
-		c.Addr,
-		c.Name,
-		true,
-		//"Asia/Shanghai"),
-		"Local")
-
-	sqlDB, err := sql.Open("mysql", dsn)
-	if err != nil {
-		log.Panicf("open mysql failed. database name: %s, err: %+v", c.Name, err)
-	}
-	// set for db connection
-	// 用于设置最大打开的连接数，默认值为0表示不限制.设置最大的连接数，可以避免并发太高导致连接mysql出现too many connections的错误。
-	sqlDB.SetMaxOpenConns(c.MaxOpenConn)
-	// 用于设置闲置的连接数.设置闲置的连接数则当开启的一个连接使用完成后可以放在池里等候下一次使用。
-	sqlDB.SetMaxIdleConns(c.MaxIdleConn)
-	sqlDB.SetConnMaxLifetime(c.ConnMaxLifeTime)
-
-	db, err := gorm.Open(mysql.New(mysql.Config{Conn: sqlDB}), &gorm.Config{})
-	if err != nil {
-		log.Panicf("database connection failed. database name: %s, err: %+v", c.Name, err)
-	}
-	db.Set("gorm:table_options", "CHARSET=utf8mb4")
-
-	DB = db
-
-	return db
+	DB = orm.NewMySQL(cfg)
+	return DB
 }
 
 // GetDB 返回默认的数据库
---
+++ pkg/database/orm/orm.go
@@ -0,0 +1,45 @@
+package orm
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/1024casts/snake/pkg/conf"
+	// MySQL driver.
+	"gorm.io/driver/mysql"
+	// GORM MySQL
+	"gorm.io/gorm"
+)
+
+// NewMySQL 链接数据库，生成数据库实例
+func NewMySQL(cfg *conf.Config) (db *gorm.DB) {
+	c := cfg.MySQL
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=%t&loc=%s",
+		c.UserName,
+		c.Password,
+		c.Addr,
+		c.Name,
+		true,
+		//"Asia/Shanghai"),
+		"Local")
+
+	sqlDB, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Panicf("open mysql failed. database name: %s, err: %+v", c.Name, err)
+	}
+	// set for db connection
+	// 用于设置最大打开的连接数，默认值为0表示不限制.设置最大的连接数，可以避免并发太高导致连接mysql出现too many connections的错误。
+	sqlDB.SetMaxOpenConns(c.MaxOpenConn)
+	// 用于设置闲置的连接数.设置闲置的连接数则当开启的一个连接使用完成后可以放在池里等候下一次使用。
+	sqlDB.SetMaxIdleConns(c.MaxIdleConn)
+	sqlDB.SetConnMaxLifetime(c.ConnMaxLifeTime)
+
+	db, err = gorm.Open(mysql.New(mysql.Config{Conn: sqlDB}), &gorm.Config{})
+	if err != nil {
+		log.Panicf("database connection failed. database name: %s, err: %+v", c.Name, err)
+	}
+	db.Set("gorm:table_options", "CHARSET=utf8mb4")
+
+	return db
+}
