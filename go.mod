ce5298090518c25c317418c77cfc5f38d2f7143b
qloog <quanlongwang@gmail.com>
2021-04-01 23:28:07 +0800

    feat: add preiod limit

--- go.mod
+++ go.mod
@@ -3,11 +3,10 @@ module github.com/1024casts/snake
 go 1.13
 
 require (
-	github.com/DATA-DOG/go-sqlmock v1.3.3
+	github.com/DATA-DOG/go-sqlmock v1.4.1
 	github.com/HdrHistogram/hdrhistogram-go v1.0.1 // indirect
 	github.com/Shopify/sarama v1.19.0
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
-	github.com/alicebob/gopher-json v0.0.0-20200520072559-a9ecdc9d1d3a // indirect
 	github.com/alicebob/miniredis v2.5.0+incompatible
 	github.com/dgraph-io/ristretto v0.0.3
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
@@ -46,6 +45,7 @@ require (
 	github.com/stretchr/testify v1.7.0
 	github.com/swaggo/gin-swagger v1.2.0
 	github.com/swaggo/swag v1.7.0
+	github.com/tal-tech/go-zero v1.1.6 // indirect
 	github.com/teris-io/shortid v0.0.0-20171029131806-771a37caa5cf
 	github.com/toolkits/net v0.0.0-20160910085801-3f39ab6fe3ce
 	github.com/uber/jaeger-client-go v2.25.0+incompatible
---
+++ pkg/ratelimit/periodlimit.go
@@ -0,0 +1,119 @@
+package ratelimit
+
+import (
+	"errors"
+	"strconv"
+	"time"
+
+	"github.com/go-redis/redis"
+)
+
+const (
+	// to be compatible with aliyun redis, we cannot use `local key = KEYS[1]` to reuse the key
+	periodLuaScript = `local limit = tonumber(ARGV[1])
+local window = tonumber(ARGV[2])
+local current = redis.call("INCRBY", KEYS[1], 1)
+if current == 1 then
+    redis.call("expire", KEYS[1], window)
+    return 1
+elseif current < limit then
+    return 1
+elseif current == limit then
+    return 2
+else
+    return 0
+end`
+	zoneDiff = 3600 * 8 // GMT+8 for our services
+)
+
+const (
+	// Unknown means not initialized state.
+	Unknown = iota
+	// Allowed means allowed state.
+	Allowed
+	// HitQuota means this request exactly hit the quota.
+	HitQuota
+	// OverQuota means passed the quota.
+	OverQuota
+
+	internalOverQuota = 0
+	internalAllowed   = 1
+	internalHitQuota  = 2
+)
+
+// ErrUnknownCode is an error that represents unknown status code.
+var ErrUnknownCode = errors.New("ratelimit: unknown status code")
+
+type (
+	// PeriodOption defines the method to customize a PeriodLimit.
+	PeriodOption func(l *PeriodLimit)
+
+	// A PeriodLimit is used to limit requests during a period of time.
+	PeriodLimit struct {
+		period     int
+		quota      int
+		limitStore *redis.Client
+		keyPrefix  string
+		align      bool
+	}
+)
+
+// NewPeriodLimit returns a PeriodLimit with given parameters.
+func NewPeriodLimit(period, quota int, limitStore *redis.Client, keyPrefix string,
+	opts ...PeriodOption) *PeriodLimit {
+	limiter := &PeriodLimit{
+		period:     period,
+		quota:      quota,
+		limitStore: limitStore,
+		keyPrefix:  keyPrefix,
+	}
+
+	for _, opt := range opts {
+		opt(limiter)
+	}
+
+	return limiter
+}
+
+// Take requests a permit, it returns the permit state.
+func (h *PeriodLimit) Take(key string) (int, error) {
+	resp, err := h.limitStore.Eval(periodLuaScript, []string{h.keyPrefix + key}, []string{
+		strconv.Itoa(h.quota),
+		strconv.Itoa(h.calcExpireSeconds()),
+	}).Result()
+	if err != nil {
+		return Unknown, err
+	}
+
+	code, ok := resp.(int64)
+	if !ok {
+		return Unknown, ErrUnknownCode
+	}
+
+	switch code {
+	case internalOverQuota:
+		return OverQuota, nil
+	case internalAllowed:
+		return Allowed, nil
+	case internalHitQuota:
+		return HitQuota, nil
+	default:
+		return Unknown, ErrUnknownCode
+	}
+}
+
+func (h *PeriodLimit) calcExpireSeconds() int {
+	if h.align {
+		unix := time.Now().Unix() + zoneDiff
+		return h.period - int(unix%int64(h.period))
+	}
+
+	return h.period
+}
+
+// Align returns a func to customize a PeriodLimit with alignment.
+func Align() PeriodOption {
+	return func(l *PeriodLimit) {
+		l.align = true
+	}
+}
