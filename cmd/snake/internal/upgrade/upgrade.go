f3a949b02dc8aaa82a4617c53f58c778648ff140
quanlong <wangquanlong@joyme.sg>
2021-04-02 20:05:25 +0800

    feat: add upgrade cmd

---
+++ cmd/snake/internal/upgrade/upgrade.go
@@ -0,0 +1,27 @@
+package upgrade
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/1024casts/snake/cmd/snake/internal/base"
+)
+
+// CmdUpgrade represents the upgrade command.
+var CmdUpgrade = &cobra.Command{
+	Use:   "upgrade",
+	Short: "Upgrade the snake tools",
+	Long:  "Upgrade the snake tools. Example: snake upgrade",
+	Run:   Run,
+}
+
+// Run upgrade the kratos tools.
+func Run(cmd *cobra.Command, args []string) {
+	err := base.GoGet(
+		"github.com/1024casts/snake/cmd/snake",
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
