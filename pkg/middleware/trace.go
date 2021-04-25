e5615ee95014a8df32e9bdea4d85790afa928005
qloog <quanlongwang@gmail.com>
2021-04-26 00:22:33 +0800

    chore: add trace id and span id

--- pkg/middleware/trace.go
+++ pkg/middleware/trace.go
@@ -1,11 +1,13 @@
 package middleware
 
 import (
+	"context"
+
+	"github.com/uber/jaeger-client-go"
+
 	"github.com/gin-gonic/gin"
 	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/ext"
-
-	"github.com/1024casts/snake/pkg/log"
 )
 
 const (
@@ -17,27 +19,50 @@ func Trace() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		tracer := opentracing.GlobalTracer()
 
+		var newCtx context.Context
 		var sp opentracing.Span
 		// for http
 		spanCtx, err := tracer.Extract(
 			opentracing.HTTPHeaders,
 			opentracing.HTTPHeadersCarrier(c.Request.Header),
 		)
-		if err != nil && err != opentracing.ErrSpanContextNotFound {
-			log.Warn("err", err)
+		if err != nil {
+			// root
+			sp, newCtx = opentracing.StartSpanFromContextWithTracer(
+				c.Request.Context(),
+				tracer,
+				c.Request.URL.Path,
+			)
+		} else {
+			sp, newCtx = opentracing.StartSpanFromContextWithTracer(
+				c.Request.Context(),
+				tracer,
+				c.Request.URL.Path,
+				opentracing.ChildOf(spanCtx),
+				opentracing.Tag{Key: string(ext.Component), Value: "HTTP"},
+			)
 		}
-		sp = tracer.StartSpan(
-			"HTTP "+c.Request.Method+" "+c.Request.URL.Path,
-			ext.RPCServerOption(spanCtx),
-			opentracing.Tag{Key: string(ext.Component), Value: "HTTP"},
-		)
 
 		// record HTTP method
 		ext.HTTPMethod.Set(sp, c.Request.Method)
 		// record HTTP url
 		ext.HTTPUrl.Set(sp, c.Request.URL.String())
 
-		c.Request = c.Request.WithContext(opentracing.ContextWithSpan(c.Request.Context(), sp))
+		// add trace id and span id
+		// get trace id and span id by using log
+		var traceID string
+		var spanID string
+		var spanContext = sp.Context()
+		switch spanContext.(type) {
+		case jaeger.SpanContext:
+			jaegerContext := spanContext.(jaeger.SpanContext)
+			traceID = jaegerContext.TraceID().String()
+			spanID = jaegerContext.SpanID().String()
+		}
+		c.Set("X-Trace-ID", traceID)
+		c.Set("X-Span-ID", spanID)
+
+		c.Request = c.Request.WithContext(newCtx)
 
 		c.Next()
 
