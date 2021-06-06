acc37928afdfdd3fec5d3a175c8986c257f9450f
qloog <quanlongwang@gmail.com>
2021-06-06 22:33:41 +0800

    init

---
+++ pkg/cache/getter.go
@@ -0,0 +1,11 @@
+package cache
+
+type Getter interface {
+	Get(key string) interface{}
+}
+
+type GetFunc func(key string) interface{}
+
+func (f GetFunc) Get(key string) interface{} {
+	return f(key)
+}
