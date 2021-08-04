e254851084c90355a95bff15b33ac78e576f922e
qloog <quanlongwang@gmail.com>
2021-08-04 20:18:43 +0800

    chore: optimize response Error

--- pkg/app/response.go
+++ pkg/app/response.go
@@ -12,6 +12,7 @@ type Response struct {
 	Code    int         `json:"code"`
 	Message string      `json:"message"`
 	Data    interface{} `json:"data"`
+	Details []string    `json:"details"`
 }
 
 func NewResponse() *Response {
@@ -27,15 +28,31 @@ func (r *Response) Success(c *gin.Context, data interface{}) {
 		Code:    errcode.Success.Code(),
 		Message: errcode.Success.Msg(),
 		Data:    data,
+		Details: []string{},
 	})
 }
 
-func (r *Response) Error(c *gin.Context, err *errcode.Error) {
-	response := gin.H{"code": err.Code(), "msg": err.Msg(), "data": gin.H{}}
-	details := err.Details()
-	if len(details) > 0 {
-		response["details"] = details
+func (r *Response) Error(c *gin.Context, err error) {
+	if err != nil {
+		if v, ok := err.(*errcode.Error); ok {
+			response := Response{
+				Code:    v.Code(),
+				Message: v.Msg(),
+				Data:    gin.H{},
+				Details: []string{},
+			}
+			details := v.Details()
+			if len(details) > 0 {
+				response.Details = details
+			}
+			c.JSON(v.StatusCode(), response)
+			return
+		}
 	}
 
-	c.JSON(err.StatusCode(), response)
+	c.JSON(http.StatusOK, Response{
+		Code:    errcode.Success.Code(),
+		Message: errcode.Success.Msg(),
+		Data:    gin.H{},
+	})
 }
