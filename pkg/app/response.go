75ab8ee38c6c557f0e6346105cefd026eacbe4c8
qloog <quanlongwang@gmail.com>
2021-09-28 23:12:13 +0800

    chore: optimize code

--- pkg/app/response.go
+++ pkg/app/response.go
@@ -33,26 +33,26 @@ func (r *Response) Success(c *gin.Context, data interface{}) {
 }
 
 func (r *Response) Error(c *gin.Context, err error) {
-	if err != nil {
-		if v, ok := err.(*errcode.Error); ok {
-			response := Response{
-				Code:    v.Code(),
-				Message: v.Msg(),
-				Data:    gin.H{},
-				Details: []string{},
-			}
-			details := v.Details()
-			if len(details) > 0 {
-				response.Details = details
-			}
-			c.JSON(v.StatusCode(), response)
-			return
-		}
+	if err == nil {
+		c.JSON(http.StatusOK, Response{
+			Code:    errcode.Success.Code(),
+			Message: errcode.Success.Msg(),
+			Data:    gin.H{},
+		})
 	}
 
-	c.JSON(http.StatusOK, Response{
-		Code:    errcode.Success.Code(),
-		Message: errcode.Success.Msg(),
-		Data:    gin.H{},
-	})
+	if v, ok := err.(*errcode.Error); ok {
+		response := Response{
+			Code:    v.Code(),
+			Message: v.Msg(),
+			Data:    gin.H{},
+			Details: []string{},
+		}
+		details := v.Details()
+		if len(details) > 0 {
+			response.Details = details
+		}
+		c.JSON(v.StatusCode(), response)
+		return
+	}
 }
