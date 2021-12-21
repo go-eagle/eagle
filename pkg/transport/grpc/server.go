568f24d4ba313ba686317ee587efb2c4d5cd9c73
qloog <quanlongwang@gmail.com>
2021-12-21 19:59:05 +0800

    chore: merge option to server

--- pkg/transport/grpc/server.go
+++ pkg/transport/grpc/server.go
@@ -19,6 +19,44 @@ import (
 	"github.com/go-eagle/eagle/pkg/utils"
 )
 
+// ServerOption is gRPC server option.
+type ServerOption func(o *Server)
+
+// Network with server network.
+func Network(network string) ServerOption {
+	return func(s *Server) {
+		s.network = network
+	}
+}
+
+// Address with server address.
+func Address(addr string) ServerOption {
+	return func(s *Server) {
+		s.address = addr
+	}
+}
+
+// Timeout with server timeout.
+func Timeout(timeout time.Duration) ServerOption {
+	return func(s *Server) {
+		s.timeout = timeout
+	}
+}
+
+// UnaryInterceptor returns a ServerOption that sets the UnaryServerInterceptor for the server.
+func UnaryInterceptor(in ...grpc.UnaryServerInterceptor) ServerOption {
+	return func(s *Server) {
+		s.inters = in
+	}
+}
+
+// Options with grpc options.
+func Options(opts ...grpc.ServerOption) ServerOption {
+	return func(s *Server) {
+		s.grpcOpts = opts
+	}
+}
+
 // Server is a gRPC server wrapper.
 // nolint
 type Server struct {
