b0a23483c7fcc75ac8bd3070c559f6a2a23c9603
Richard <quanlongwang@gmail.com>
2021-12-31 22:44:41 +0800

    style: format

--- cmd/eagle/internal/proto/server/template.go
+++ cmd/eagle/internal/proto/server/template.go
@@ -8,6 +8,7 @@ import (
 var serviceTemplate = `
 {{- /* delete empty line */ -}}
 package service
+
 import (
 	{{- if .UseContext }}
 	"context"
@@ -15,17 +16,21 @@ import (
 	{{- if .UseIO }}
 	"io"
 	{{- end }}
+
 	pb "{{ .Package }}"
 	{{- if .GoogleEmpty }}
 	"google.golang.org/protobuf/types/known/emptypb"
 	{{- end }}
 )
+
 type {{ .Service }}Service struct {
 	pb.Unimplemented{{ .Service }}Server
 }
+
 func New{{ .Service }}Service() *{{ .Service }}Service {
 	return &{{ .Service }}Service{}
 }
+
 {{- $s1 := "google.protobuf.Empty" }}
 {{ range .Methods }}
 {{- if eq .Type 1 }}
@@ -33,6 +38,7 @@ func (s *{{ .Service }}Service) {{ .Name }}(ctx context.Context, req {{ if eq .R
 {{ else }}*pb.{{ .Request }}{{ end }}) ({{ if eq .Reply $s1 }}*emptypb.Empty{{ else }}*pb.{{ .Reply }}{{ end }}, error) {
 	return {{ if eq .Reply $s1 }}&emptypb.Empty{}{{ else }}&pb.{{ .Reply }}{}{{ end }}, nil
 }
+
 {{- else if eq .Type 2 }}
 func (s *{{ .Service }}Service) {{ .Name }}(conn pb.{{ .Service }}_{{ .Name }}Server) error {
 	for {
@@ -50,6 +56,7 @@ func (s *{{ .Service }}Service) {{ .Name }}(conn pb.{{ .Service }}_{{ .Name }}Se
 		}
 	}
 }
+
 {{- else if eq .Type 3 }}
 func (s *{{ .Service }}Service) {{ .Name }}(conn pb.{{ .Service }}_{{ .Name }}Server) error {
 	for {
@@ -62,6 +69,7 @@ func (s *{{ .Service }}Service) {{ .Name }}(conn pb.{{ .Service }}_{{ .Name }}Se
 		}
 	}
 }
+
 {{- else if eq .Type 4 }}
 func (s *{{ .Service }}Service) {{ .Name }}(req {{ if eq .Request $s1 }}*emptypb.Empty
 {{ else }}*pb.{{ .Request }}{{ end }}, conn pb.{{ .Service }}_{{ .Name }}Server) error {
