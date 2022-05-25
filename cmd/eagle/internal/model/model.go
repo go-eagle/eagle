ea23d496f1a7c32076e414325eba92f024d81e7f
qloog <quanlongwang@gmail.com>
2022-05-25 22:27:09 +0800

    feat: add gen cmd for model

---
+++ cmd/eagle/internal/model/model.go
@@ -0,0 +1,162 @@
+package model
+
+import (
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/spf13/cobra"
+)
+
+// CmdNew represents the proto command.
+var CmdNew = &cobra.Command{
+	Use:   "model",
+	Short: "Generate the model file.",
+	Long:  "Generate the model file via database table.",
+	Run:   run,
+}
+
+var (
+	filename    string
+	host        string
+	database    string
+	tableName   string
+	packageName string
+	structName  string
+	user        string
+	password    string
+	format      string
+	targetDir   string
+)
+
+func init() {
+	// default value
+	host = "localhost"
+	packageName = "model"
+	user = "root"
+	password = "123456"
+	targetDir = "internal/model"
+
+	CmdNew.Flags().StringVarP(&filename, "filename", "f", filename, "model filename")
+	CmdNew.Flags().StringVar(&host, "host", host, "database host addr")
+	CmdNew.Flags().StringVarP(&database, "database", "d", database, "database name")
+	CmdNew.Flags().StringVarP(&tableName, "table", "t", tableName, "table name")
+	CmdNew.Flags().StringVarP(&user, "user", "u", user, "database username")
+	CmdNew.Flags().StringVarP(&password, "password", "p", password, "password for database")
+	CmdNew.Flags().StringVar(&packageName, "package", packageName, "package name")
+	CmdNew.Flags().StringVarP(&structName, "struct", "s", structName, "model struct name")
+	CmdNew.Flags().StringVar(&format, "format", format, "add json annotations (default)")
+	CmdNew.Flags().StringVar(&targetDir, "target-dir", targetDir, "model target dir")
+}
+
+func run(cmd *cobra.Command, args []string) {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	// TODO: read database config
+
+	// get file name
+	if len(filename) == 0 {
+		prompt := &survey.Input{
+			Message: "What is file name ?",
+			Help:    "Created file name.",
+		}
+		err = survey.AskOne(prompt, &filename)
+		if filename == "" || err != nil {
+			return
+		}
+	}
+	if len(host) == 0 {
+		prompt := &survey.Input{
+			Message: "What is host for database?",
+			Help:    "Created host for database.",
+			Default: host,
+		}
+		err = survey.AskOne(prompt, &host)
+		if host == "" || err != nil {
+			return
+		}
+	}
+	if len(database) == 0 {
+		prompt := &survey.Input{
+			Message: "What is database name ?",
+			Help:    "Created database name.",
+		}
+		err = survey.AskOne(prompt, &database)
+		if database == "" || err != nil {
+			return
+		}
+	}
+	if len(tableName) == 0 {
+		prompt := &survey.Input{
+			Message: "What is table name ?",
+			Help:    "Created table name.",
+		}
+		err = survey.AskOne(prompt, &tableName)
+		if tableName == "" || err != nil {
+			return
+		}
+	}
+	if len(user) == 0 {
+		prompt := &survey.Input{
+			Message: "What is user for database?",
+			Help:    "Created user name.",
+			Default: user,
+		}
+		err = survey.AskOne(prompt, &user)
+		if user == "" || err != nil {
+			return
+		}
+	}
+	if len(password) == 0 {
+		prompt := &survey.Input{
+			Message: "What is password for database?",
+			Help:    "Created password.",
+		}
+		err = survey.AskOne(prompt, &password)
+		if user == "" || err != nil {
+			return
+		}
+	}
+	if len(packageName) == 0 {
+		prompt := &survey.Input{
+			Message: "What is package name for model?",
+			Help:    "Created package name.",
+			Default: packageName,
+		}
+		err = survey.AskOne(prompt, &packageName)
+		if packageName == "" || err != nil {
+			return
+		}
+	}
+	if len(structName) == 0 {
+		prompt := &survey.Input{
+			Message: "What is struct name for model?",
+			Help:    "Created struct name.",
+		}
+		err = survey.AskOne(prompt, &structName)
+		if filename == "" || err != nil {
+			return
+		}
+	}
+
+	m := &Model{
+		Filename:    filename,
+		Host:        host,
+		Database:    database,
+		TableName:   tableName,
+		PackageName: packageName,
+		StructName:  structName,
+		User:        user,
+		Password:    password,
+		Format:      format,
+		TargetDir:   path.Join(wd, targetDir),
+	}
+	if err := m.Generate(); err != nil {
+		fmt.Println(err)
+		return
+	}
+}
---
+++ cmd/eagle/internal/model/new.go
@@ -0,0 +1,118 @@
+package model
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+
+	"github.com/go-eagle/eagle/cmd/eagle/internal/base"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/fatih/color"
+)
+
+// Model is a model template.
+type Model struct {
+	Filename    string
+	Host        string
+	Database    string
+	TableName   string
+	PackageName string
+	StructName  string
+	User        string
+	Password    string
+	Format      string
+	TargetDir   string
+}
+
+// New new a project from remote repo.
+func (m *Model) Generate() error {
+	to := path.Join(m.TargetDir, m.Filename)
+	if _, err := os.Stat(to); !os.IsNotExist(err) {
+		fmt.Printf("🚫 %s already exists\n", m.Filename)
+		override := false
+		prompt := &survey.Confirm{
+			Message: "📂 Do you want to override the file ?",
+			Help:    "Delete the existing file and create the file.",
+		}
+		e := survey.AskOne(prompt, &override)
+		if e != nil {
+			return e
+		}
+		if !override {
+			return err
+		}
+		e = os.RemoveAll(to)
+		if e != nil {
+			return e
+		}
+	}
+
+	var (
+		err error
+	)
+	// check db2struct if installed
+	if err = look("db2struct"); err != nil {
+		// install db2struct
+		err := base.GoInstall(
+			"github.com/Shelnutt2/db2struct/cmd/db2struct",
+		)
+		if err != nil {
+			return err
+		}
+	}
+
+	fmt.Printf("🚀 Creating model %s, please wait a moment.\n", m.Filename)
+
+	input := []string{
+		"--host", m.Host,
+		"--database", m.Database,
+		"--table", m.TableName,
+		"--package", m.PackageName,
+		"--struct", m.StructName,
+		"--user", m.User,
+		"-p", m.Password,
+		"--gorm",
+	}
+
+	// default to add json
+	if len(m.Format) == 0 {
+		input = append(input, "--json")
+	} else {
+		// --no-json
+		input = append(input, m.Format)
+	}
+
+	// read stdout to buf
+	var (
+		stdout bytes.Buffer
+	)
+	cmd := exec.Command("db2struct", input...)
+	cmd.Stdout = &stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		_, err = fmt.Fprintf(os.Stderr, "\033[31mERROR: failed to generate model(%s)\033[m\n", err.Error())
+		return err
+	}
+
+	// write stdout to file
+	if err = os.WriteFile(to, stdout.Bytes(), 0644); err != nil {
+		_, err = fmt.Fprintf(os.Stderr, "\033[31mERROR: write to model(%s) error\033[m\n", err.Error())
+		return err
+	}
+
+	fmt.Printf("\n🍺 Model creation succeeded %s\n", color.GreenString(m.Filename))
+	return nil
+}
+
+// look find executable file
+func look(name ...string) error {
+	for _, n := range name {
+		if _, err := exec.LookPath(n); err != nil {
+			return err
+		}
+	}
+	return nil
+}
