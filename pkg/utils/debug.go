86684b743ffe87d1330b941e39dfa3cc6cc492be
Richard <quanlongwang@gmail.com>
2024-10-29 23:23:32 +0800

    feat: add new func async.Go and Stringify

--- pkg/utils/debug.go
+++ pkg/utils/debug.go
@@ -5,8 +5,8 @@ import (
 	"runtime"
 )
 
-// PrintStackTrace print stack info
-func PrintStackTrace(msg string, err interface{}) string {
+// StackTrace return stack info
+func StackTrace(msg string, err interface{}) string {
 	buf := make([]byte, 64*1024)
 	buf = buf[:runtime.Stack(buf, false)]
 	return fmt.Sprintf("%s, err: %s\nstack: %s", msg, err, buf)
